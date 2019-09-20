85ef67a8b15e301abd0c194e08ee3d26f2a297c7
40823218 <40823218@gm.nfu.edu.tw>
2019-09-20 23:16:35 +0800

    initial add

---
+++ content/tipuesearch_content.js
@@ -0,0 +1,1 @@
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'http://mde.tw/cp2019 \n \n 123.pdf', 'tags': '', 'url': 'About.html'}, {'title': 'USB', 'text': '', 'tags': '', 'url': 'USB.html'}, {'title': 'USB2', 'text': '', 'tags': '', 'url': 'USB2.html'}]};
